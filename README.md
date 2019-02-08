adf3e0b0d54bec3df8b4f4b031ec51182448cfc4
Kris Watson <kris@computestacks.com>
2019-02-07 17:39:52 -0800

    Formatting for txt fields

--- README.md
+++ README.md
@@ -1,22 +1,23 @@
 # AutoDNS Module for ComputeStacks
 
 
-#### Example:
+#### Examples:
 
-Load a Zone
-```
+
+```ruby
+##
+# Load a Zone
+  
 auth = AutoDNS::Auth.new(0, 'USER', 'PASS', 'CONTEXT')
 client = AutoDNS::Client.new(nil, auth)
-zone = AutoDNS::Dns::Zone.find(client, 'usr.cloud')
-```
+zone = AutoDNS::Dns::Zone.find(client, 'kwtester.net')
 
-Create a Zone
-```
-auth = AutoDNS::Auth.new(0, 'USER', 'PASS', 'CONTEXT')
-client = AutoDNS::Client.new(nil, auth)
+##
+# Create a Zone
 zone = AutoDNS::Dns::Zone.new(client, nil)
 zone.name = 'cmptstks.com'
 zone.soa_email = 'dns@computestacks.com'
 zone.default_ip = '62.116.130.8'
 zone
+
 ```
--- lib/autodns/dns/zone_record.rb
+++ lib/autodns/dns/zone_record.rb
@@ -30,47 +30,66 @@ module AutoDNS::Dns
       self.name = data['name'].nil? ? zone_id : data['name']
       self.ttl = data['ttl'] if data['ttl']
       self.type = data['type']
-      data['value'] = data['value'].gsub('"', '').strip
       case self.type
       when 'CNAME', 'NS'
-        self.hostname = data['value']
+        self.hostname = data['value'].gsub('"', '').strip
       when 'A', 'AAAA'
-        self.ip = data['value']
+        self.ip = data['value'].gsub('"', '').strip
       when 'MX'
         self.priority = data['pref']
-        self.hostname = data['value']
+        self.hostname = data['value'].gsub('"', '').strip
       when 'NS'
-        self.hostname = data['value']
+        self.hostname = data['value'].gsub('"', '').strip
+      else # TXT, PTR
+        if data['value'] && data['value'].length > 1
+          if data['value'].split("\"").count > 1 # Split record?
+            self.value = data['value'].split("\"").map {|i| i.strip unless i.length.zero?}.join("").gsub('\"','').gsub('"','')
+          else
+            self.value = data['value'].gsub('\"', '').gsub('"','').strip
+          end
+        end
+      end
+    end
+
+    # Clean up values in preperation for sending to AutoDNS.
+    def format!
+      return true unless self.value && self.value.length > 0
+      case self.type
       when 'TXT'
+        self.value = self.value.strip
         # For longer strings, partition into multiple "" sections.
-        val = data['value']
-        self.value = val&.length > 254 ? "\"#{val[0..254]}\" \"#{val[255..-1]}\"" : val
-      else PTR
-        self.value = data['value'].gsub('"', '').strip
+        if self.value.length > 254 && self.value.split("\"").count < 2
+          self.value = "\"#{self.value[0..254]}\" \"#{self.value[255..-1]}\""
+        else # Ensure we have quotes around our string.
+          self.value = self.value.gsub('\"','').gsub('"','')
+          self.value = "\"#{self.value}\""
+        end
+      when 'PTR'
+        self.value = self.value.gsub('"', '').strip
       end
     end
 
     # Format record value specifically for PowerDNS.
-    # TODO: Is this still in use?
-    def raw_value
-      case self.type
-      when 'CNAME', 'NS'
-        self.hostname
-      when 'A', 'AAAA'
-        self.ip
-      when 'MX'
-        "#{self.priority} #{self.hostname}"
-      when 'TXT'
-        # Strip quotes
-        self.value = self.value.gsub('"', '').strip
-        # For longer strings, partition into multiple "" sections.
-        self.value = "\"#{self.value[0..254]}\" \"#{self.value[255..-1]}\"" if self.value.length > 254
-      else # PTR
-        self.value.strip
-      end
-    end
+    # def raw_value
+    #   case self.type
+    #   when 'CNAME', 'NS'
+    #     self.hostname
+    #   when 'A', 'AAAA'
+    #     self.ip
+    #   when 'MX'
+    #     "#{self.priority} #{self.hostname}"
+    #   when 'TXT'
+    #     # Strip quotes
+    #     self.value = self.value.gsub('"', '').strip
+    #     # For longer strings, partition into multiple "" sections.
+    #     self.value = "\"#{self.value[0..254]}\" \"#{self.value[255..-1]}\"" if self.value.length > 254
+    #   else # PTR
+    #     self.value.strip
+    #   end
+    # end
 
     def raw_record
+      format!
       case self.type
       when 'NS', 'MX'
         <<EOF
@@ -101,7 +120,16 @@ EOF
 </rr>
 EOF
 
-      else # TXT, PTR
+      when 'TXT'
+        <<EOF
+<rr>
+  <name>#{self.name}</name>
+  <ttl>#{self.ttl}</ttl>
+  <type>#{self.type}</type>
+  <value>#{self.value}</value>
+</rr>
+EOF
+      else # PTR
         <<EOF
 <rr>
   <name>#{self.name}</name>
